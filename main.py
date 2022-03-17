2cc377a013e6115a9e00fcec96c01fd33215e41d
JanHoly <53258234+hongolem@users.noreply.github.com>
2022-03-17 11:12:51 +0100

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 radio.set_group(250)
 RunComp.set_light_level()
 
-key0 = 0
-key1 = 1
-key2 = 2
+reset = 0
+start = 1
+stop = 2
 
 
 def on_light_drop():
-    radio.send_number(key1)
+    radio.send_number(start)
 RunComp.on_light_drop(on_light_drop)
 
 def on_button_pressed_a():
-    radio.send_number(key0)
+    radio.send_number(reset)
     control.reset()
 input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_received_number1(key2):
-    control.reset()
-radio.on_received_string(on_received_number1)
-
-def on_received_number2(key0):
-    control.reset()
-radio.on_received_string(on_received_number2)
--- main.ts
+++ main.ts
@@ -1,18 +1,12 @@
 radio.setGroup(250)
 RunComp.SetLightLevel()
-let key0 = 0
-let key1 = 1
-let key2 = 2
+let reset = 0
+let start = 1
+let stop = 2
 RunComp.OnLightDrop(function on_light_drop() {
-    radio.sendNumber(key1)
+    radio.sendNumber(start)
 })
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    radio.sendNumber(key0)
-    control.reset()
-})
-radio.onReceivedString(function on_received_number1(key2: string) {
-    control.reset()
-})
-radio.onReceivedString(function on_received_number2(key0: string) {
+    radio.sendNumber(reset)
     control.reset()
 })
